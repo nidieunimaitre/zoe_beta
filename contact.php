3767453a0477e894fd92c7761dcdf7e3f5b0470c
raphael <raphael.guetlin@gmail.com>
2016-09-30 15:01:00 +0200

    contact

--- contact.php
+++ contact.php
@@ -4,57 +4,62 @@
 		$email = $_POST['email'];
 		$message = $_POST['message'];
 		$human = intval($_POST['human']);
-		$from = "Contactform";
+		$from = "zoe.wtf message";
 		$to = 'raphael.guetlin@gmail.com';
-		$subject = 'Message From Contactform';
+		$subject = 'message from zoe.wtf';
 
 		$body ="From: $name\n E-Mail: $email\n Message:\n $message";
 		// Check if name has been entered
 		if (!$_POST['name']) {
-			$errName = 'Please enter your name';
+			$errName = 'please enter your name';
 		};
 
 		// Check if email has been entered and is valid
 		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-			$errEmail = 'Please enter a valid email address';
+			$errEmail = 'please enter a valid email address';
 		};
 
 		//Check if message has been entered
 		if (!$_POST['message']) {
-			$errMessage = 'Please enter your message';
+			$errMessage = 'please enter ';
 		};
 		//Check if simple anti-bot test is correct
 		if ($human !== 5) {
-			$errHuman = 'Your anti-spam is incorrect';
+			$errHuman = 'your anti-spam is incorrect';
 		};
     // If there are no errors, send the email
     if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
     	if (mail($to, $subject, $body, $from)) {
-    		$result='Message sent.';
+    		$result='message sent.';
     	} else {
-    		$result='Sorry there was an error sending your message. Please try again later.';
+    		$result='sorry there was an error sending your message. please try again later.';
     	};
     };
 	};
 ?>
 
 <div class="contactform">
-  <form action="index.php?page=contact&pagetype=main&showtext=1" method="post">
+<?php if (!$result): ?>
+	<form action="index.php?page=contact&pagetype=main&showtext=1" method="post">
 
     <input type="text" name="name" placeholder="name" value="<?php echo htmlspecialchars($_POST['name']); ?>" />
-    <?php echo " $errName";?><br><br>
+    <?php echo $errName;?><br><br>
 
-    <input type="text" name="email" placeholder="yourname@mail.com" value="<?php echo htmlspecialchars($_POST['name']); ?>" />
-    <?php echo " $errEmail";?><br><br>
+    <input type="text" name="email" placeholder="yourname@mail.com" value="<?php echo htmlspecialchars($_POST['email']); ?>" />
+    <?php echo $errEmail;?><br><br>
 
-    <textarea name="message" placeholder="your message"><?php echo htmlspecialchars($_POST['message']);?></textarea>
-    <?php echo " $errMessage";?><br><br>
+		<?php $placeholderMessage = $errMessage.'your message'; ?>
+    <textarea name="message" placeholder="<?php echo $placeholderMessage; ?>"><?php echo htmlspecialchars($_POST['message']);?></textarea>
+    <!--<?php echo $errMessage;?><br>--><br>
 
     <input type="text" name="human" placeholder="2 + 3 = ?" />
-    <?php echo " $errName";?><br><br>
+    <?php echo $errHuman;?><br><br>
 
-    <input type="submit" value="send" class="hvr-wobble-skew "/><br><br>
+    <input type="submit" name="submit" value="send" class="hvr-wobble-skew "/><br><br>
 
-    <?php echo $result; ?>
   </form>
+<?php else: ?>
+	<?php echo $result; ?>
+<?php endif; ?>
+
 </div>
